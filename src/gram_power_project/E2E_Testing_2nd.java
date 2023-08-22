02ef44c2e0c7c7af5e25a329e073787de3705fe2
green <green@LAPTOP-ANBE3D39>
2023-08-22 15:26:44 +0530

    Secondcommit

--- src/gram_power_project/E2E_Testing_2nd.java
+++ src/gram_power_project/E2E_Testing_2nd.java
@@ -1,10 +1,52 @@
 package gram_power_project;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class E2E_Testing_2nd {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.gecko.driver","C:\\Tools\\geckodriver-v0.32.2-win32\\geckodriver.exe");
+		 WebDriver Driver = new FirefoxDriver();	
+		 
+		 Driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		 
+		 Driver.get("https://data.grampower.com/hes/");
+		 
+		 Driver.findElement(By.xpath("//input[@name='username']")).sendKeys("automationuser");
+		 Driver.findElement(By.xpath("//input[@name='password']")).sendKeys("grampower");
+		 Driver.findElement(By.xpath("//input[@value='LOG IN']")).click();
+		 
+		 Driver.findElement(By.xpath("//button[text()='Later']")).click();
+		
+		 try {
+			 Driver.findElement(By.xpath("//div[@class='arrow-next']")).click();
+		} catch (NoSuchElementException e) {
+			
+		}
+		 
+		 Driver.findElement(By.xpath("//div[@class='arrow-next']")).click();
+		 
+		 Driver.findElement(By.xpath("//div[@id='suggestion_box']/child::div/child::label")).click();
+		 
+		 Driver.findElement(By.xpath("//div[@title='ROFANANDA A']")).click();
+		 
+		 Driver.findElement(By.xpath("(//div[@aria-label='6'])[1]")).click();
+		 
+		 List<WebElement> Graph = Driver.findElements(By.xpath("(//*[local-name()='svg' and @aria-label='A chart.'])[2]//*[name()='rect']"));
+		 Actions Act=new Actions(Driver);
+		 
+		for (WebElement A : Graph) {
+			Act.moveToElement(A);
+			
+		}
 	}
 
 }
--- src/gram_power_project/E2E_Testing_Grampower.java
+++ src/gram_power_project/E2E_Testing_Grampower.java
@@ -2,17 +2,52 @@ package gram_power_project;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class E2E_Testing_Grampower {
 
 	public static void main(String[] args) {
-		
-		System.setProperty("webdriver.chrome.driver","C:\\Tools\\chromedriver\\chromedriver.exe");
-		
+		System.setProperty("webdriver.gecko.driver","C:\\Tools\\geckodriver-v0.32.2-win32\\geckodriver.exe");
+		 WebDriver Driver = new FirefoxDriver();	
 		 
 		 Driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		 
+		 Driver.get("https://data.grampower.com/hes/");
+		 
+		 Driver.findElement(By.xpath("//input[@name='username']")).sendKeys("automationuser");
+		 Driver.findElement(By.xpath("//input[@name='password']")).sendKeys("grampower");
+		 
+		 Driver.findElement(By.xpath("//input[@value='LOG IN']")).click();
+		 
+		 WebDriverWait Wait=new WebDriverWait(Driver, 20);
+		 WebElement Pop_up= Driver.findElement(By.xpath("//button[text()='Later']"));
+		 Wait.until(ExpectedConditions.visibilityOf(Pop_up));
+		 Pop_up.click();
+			
+		 try {
+			  WebElement Total_meters = Driver.findElement(By.xpath("//div[@class='sitewrap clearfix']/descendant::li[text()='ROFANANDA A ']"));
+			  System.out.println("Total Meters= "+Total_meters.getText());
+				
+		      } catch (NoSuchElementException e) {
+			
+		}
+		 try {
+			 WebElement Assined_Meter =Driver.findElement(By.xpath("//div[@id='div_assign_count']"));
+			 System.out.println("Assined Meters= "+Assined_Meter.getText());
+		} catch (NoSuchElementException e) {
+			
+		}
+	
+		 
+	
+		 
 	}
+
 
 }
